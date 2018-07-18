a67a1fdca7300b72ec099786d7c15961b7acda37
lscannon <40803293+lscannon@users.noreply.github.com>
2018-07-18 13:52:13 -0700

    Add files via upload

---
+++ Space_Invaders-master/config.py
@@ -0,0 +1,9 @@
+from pygame import *
+import sys
+from random import shuffle, randrange, choice
+
+enemyPositionDefault = 65 #65
+shipSpeed = 30 # 10
+bulletSpeed = 20 #15
+enSpeed = 30 #5
+enBulletSpeed = 700 #700 - lower makes it faster
